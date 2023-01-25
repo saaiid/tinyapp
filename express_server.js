5e6a63d0a23134ef1aefcec622d2f3b18d7035c6
saaiid <ibrosakid@gmail.com>
2023-01-25 18:10:08 -0500

    server tiny app

--- express_server.js
+++ express_server.js
@@ -14,6 +14,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  const shortURL = req.params.id
+  const templateVars = {
+    id: shortURL,
+    longURL: urlDatabase [shortURL]
+  }
+  res.render("urls_show", templateVars)
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
