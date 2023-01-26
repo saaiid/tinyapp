c849158d9b6eb5e22be45c960c54758981c7c3eb
saaiid <ibrosakid@gmail.com>
2023-01-25 21:41:54 -0500

    server tiny app

--- express_server.js
+++ express_server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 const PORT = 8080; // default port 8080
+
+generateRandomString()
+function generateRandomString() {
+  let randomString = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz';
+  for(let i = 0; i < characters.length; i++){
+    randomString+= characters.charAt(Math.floor(Math.random() * characters.length))
+  }
+  console.log(randomString);
+}
+
 
 app.set("view engine", "ejs");
 
@@ -12,6 +24,15 @@ const urlDatabase = {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  const longURL = req.body.longURL
+  console.log(req.body); // Log the POST request body to the console
+})
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -34,3 +55,4 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
