36d896f0620ade6de5a409bb920aa8066ae03bcd
saaiid <ibrosakid@gmail.com>
2023-01-28 12:24:50 -0500

    adding username to each page

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
+const cookieParser = require('cookie-parser')
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 const PORT = 8080; // default port 8080
 
 
@@ -37,8 +39,9 @@ app.get("/u/:id", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  console.log(req.cookies["username"])
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  res.render("urls_index", templateVars)
 });
 
 app.post("/urls", (req, res) => {
@@ -52,15 +55,21 @@ app.post("/urls", (req, res) => {
 
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
+  
 });
 
 app.get("/urls/:id", (req, res) => {
   const shortURL = req.params.id
   const templateVars = {
     id: shortURL,
-    longURL: urlDatabase [shortURL]
+    longURL: urlDatabase [shortURL],
+    username: req.cookies["username"],
   }
+
   res.render("urls_show", templateVars)
 })
 
